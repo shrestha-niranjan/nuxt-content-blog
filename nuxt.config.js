ac65ab1a0e65adf1fad5e1fd5c2963347620d528
Niranjan Shrestha <niranjanshrestha@Niranjans-MBP.lan>
2023-01-26 23:51:40 +0545

    making seo friendly

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,15 +6,19 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - nuxt-content-blog',
-    title: 'nuxt-content-blog',
+    title: "Niranjan Shrestha's Blog ☕☕",
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'Writing about PHP, Javascript, Jquery, HTML, CSS, Backend, Frontend, Laravel, NUXT, Vue, Tailwind'
+      },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
